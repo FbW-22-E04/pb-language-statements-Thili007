17c622455826f49a7bf657f207164cee62812b24
Your Name <tpunchihewage@gmail.com>
2022-06-30 12:26:52 +0200

    added solution.js file

---
+++ __tests__/solution.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+console.log("This is my first js file");
+
+// answer - 1
+
+/* no it gives an error*/
+
+// answer - 2
+/* its gives an error called "Unexpected string" */
